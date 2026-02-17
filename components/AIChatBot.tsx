05a003e7fe6e034b69173c4fcf3b097f3bf70bb2
Daniel <danielr16482@gmail.com>
2026-02-16 17:26:54 -0800

    replace with new design (html only)

--- components/AIChatBot.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-const AIChatBot: React.FC = () => {
-  // AI Chatbot is disabled to keep the portfolio professional and lightweight.
-  return null;
-};
-
-export default AIChatBot;
